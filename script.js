269f6770a1e29023f37610a7f5f96ee68d037a23
Jesse Rate <119457689+AngelinaMinshall@users.noreply.github.com>
2023-10-19 13:35:11 -0700

    html

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+
+Ten fat sausages in the frying pan.
+One went pop, one went bam!
+Eight fat sausages in the frying pan.
+One went pop, one went bam!
+Six fast sausages in the frying pan.
+One went pop, one went bam!
+Four fat sausages in the frying pan.
+One went pop, one went bam!
+Two fat sausages in the frying pan.
+One went pop, one went bam!
+No more sausages.
+
+I’m still hungry!
+
+*/
